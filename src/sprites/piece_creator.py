b54fff4c6719fcc1db6e725abcad9ac9dfaee858
Jere Nuora <jere.nuora@gmail.com>
2021-11-27 18:59:28 +0200

    added different piece shapes

--- src/sprites/piece_creator.py
+++ src/sprites/piece_creator.py
@@ -2,7 +2,12 @@ import pygame
 from sprites.block import Block
 
 
-SHAPES = {"SHAPE_I": [0, 0, 0, 32, 0, 64, 0, 96]}
+SHAPES = {
+    "SHAPE_I": [0, 0, 0, 32, 0, 64, 0, 96],
+    "SHAPE_L": [0, 0, 0, 32, 0, 64, 32, 64],
+    "SHAPE_T": [0, 0, 0, 32, 0, 64, 32, 32],
+    "SHAPE_SQ": [0, 0, 0, 32, 32, 0, 32, 32]
+    }
 
 
 def creator(shape, spot_x, spot_y):
